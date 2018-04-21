45b7d12f6d0a3a1f22303754cbec7a5bf553f8c3
Khang Huynh <khanghuynh92@gmail.com>
2018-04-21 16:55:40 +0700

    init source

---
+++ server/src/index.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressValidator = require('express-validator');
+const bodyParser = require('body-parser');
+const path = require('path');
+const { NOT_FOUND } = require('http-status-codes');
+
+const { sendNotFound } = require('./utils/httpError');
+
+const issuesRoute = require('./issues');
+
+// Constants
+const PORT = process.env.PORT || 9000;
+const HOST = '0.0.0.0';
+
+const CLIENT_BUILD_PATH = path.join(__dirname, '../../client/build');
+
+// App
+const app = express();
+
+// Static files
+app.use(express.static(CLIENT_BUILD_PATH));
+
+app.use(expressValidator());
+app.use(bodyParser.json());
+
+app.use('/api', [
+  issuesRoute,
+]);
+
+const notFoundError = (req, res) => res.send(NOT_FOUND, sendNotFound());
+
+// Index request return the React app, so it can handle routing.
+app.get('/', (request, response) => {
+  response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
+});
+
+app.all('*', notFoundError);
+
+
+app.listen(PORT, HOST);
+
+console.log(`Running on http://${HOST}:${PORT}`); // eslint-disable-line
---
+++ server/src/issues/controller.js
@@ -0,0 +1,9 @@
+const { OK, BAD_REQUEST } = require('http-status-codes');
+
+const list = (req, res) => {
+
+};
+
+module.exports = {
+  list,
+}
